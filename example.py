79f855b283fd75dca8a4b4d78fc540889cb36b1b
msprabha <94337565+msprabha@users.noreply.github.com>
2022-06-16 07:50:14 -0700

    Update example.py

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 def add(a,b):
     return a+b
   
-  def test_add():
+def test_add():
     assert add(2,3) == 5
     assert add('space','ship') == 'spaceship'
     
-    def subtract(a,b):
-      return a-b
+def subtract(a,b):
+    return a-b
     
-    def test_subtract(a,b):
-      assert subtract(2,3) = -1
+def test_subtract(a,b):
+    assert subtract(2,3) = -1
       
     
